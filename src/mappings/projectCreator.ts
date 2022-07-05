aa0731fb2d99d46468231f2003c03e5fda1fdee0
george-e-d-g-e <george@e-d-g-e.co.uk>
2022-07-05 13:33:57 +0100

    ts/projectCreator: index creatorApprovals

--- src/mappings/projectCreator.ts
+++ src/mappings/projectCreator.ts
@@ -4,19 +4,21 @@ import {
 } from '../../types/templates'
 import {
   CreatedProject,
-  // CreatorApprovalsUpdated
+  CreatorApprovalsUpdated
 } from '../../types/ProjectCreator/ProjectCreator'
 import {
+  findOrCreateProjectCreator,
   findOrCreateProject,
-  findOrCreateUser
+  findOrCreateUser,
+  zeroAddress
 } from "../helpers"
 import { projectImplementations } from '../constants'
-import { log, DataSourceContext, BigInt } from '@graphprotocol/graph-ts'
+import { log, DataSourceContext, BigInt, dataSource } from '@graphprotocol/graph-ts'
 
 export function handleCreatedProject (event: CreatedProject): void {
 
   let projectAddress = event.params.project.toHexString()
-  log.info(`Starting: handleCreatedProject`, [projectAddress])
+  log.info(`Starting: handleCreatedProject {}`, [projectAddress])
 
   // create new context
   let context = new DataSourceContext()
@@ -57,9 +59,24 @@ export function handleCreatedProject (event: CreatedProject): void {
 
   project.save()
 
-  log.info(`Completed: handleCreatedProject`, [projectAddress])
+  log.info(`Completed: handleCreatedProject {}`, [projectAddress])
 }
 
-// export function handleCreatorApprovalsUpdated (event: CreatorApprovalsUpdated): void {
-  
-// }
+export function handleCreatorApprovalsUpdated (event: CreatorApprovalsUpdated): void {
+  log.info(`Starting: handleCreatorApprovalsUpdated`, [])
+
+  event.params.creators.forEach(creator => {
+    let user = findOrCreateUser(creator.id.toHexString())
+    user.curatedCreator = creator.approval
+    user.save()
+
+    // update is open status
+    if(creator.id.toHexString() == zeroAddress){
+      let projectCreator = findOrCreateProjectCreator(dataSource.address().toHexString())
+      projectCreator.isOpen = creator.approval
+      projectCreator.save()
+    }
+  })
+
+  log.info(`completed: handleCreatorApprovalsUpdated`, [])
+}
